761c0f91155ef5d1806b95bf88e0471b27322bd2
hwiram <djmjy2@naver.com>
2023-09-21 17:16:24 +0900

    9/21 수업내용

---
+++ 0921/function_demo2.py
@@ -0,0 +1,10 @@
+student = { }
+
+def input_(man):
+   name = input("Enter your Name : ")
+   age = input("Enter your age : ")
+   man["name"] = name
+   man["age"] = age
+
+input_(student)
+print(student)
